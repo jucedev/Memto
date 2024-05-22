7f6bb069ef295688f3255815442a4f9c2c980376
Juce <iamjuceuwu@gmail.com>
2024-05-22 19:28:55 +0100

    Added demo

---
+++ Memto.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿var originator = new Memto.Originator<string>();
+var caretaker = new Memto.Caretaker<string>();
+
+originator.SetState("First");
+caretaker.SaveState(originator.SaveStateToMemento());
+
+originator.SetState("Second");
+caretaker.SaveState(originator.SaveStateToMemento());
+
+originator.SetState("Third");
+
+Console.WriteLine("Current state: " + originator.State);
+
+while (!caretaker.IsEmpty)
+{
+    originator.GetStateFromMemento(caretaker.RestoreState());
+    Console.WriteLine("Restored state: " + originator.State);
+}
